8aae8f3e1c4cdbaaa4c32b1f5965d1692b29a8ee
Maria Sultana Trisha <140950565+Trishannika@users.noreply.github.com>
2023-08-18 19:31:43 +0600

    Add files via upload

---
+++ Task-6.2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+
+    int r,c,n;
+    scanf("%d",&n);
+    for(r=1;r<=n;r++)
+    {
+        for(c=1;c<r;c++)
+        {
+            printf("_");
+        }
+        for(c=n;c>=r;c--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+
+
+
+
+
+
+
+}
